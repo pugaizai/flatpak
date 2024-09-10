acec115f07f556d91602bbbf49e746f205c365e9
pugaizai <i@pugai.life>
2024-09-10 19:55:30 +0800

    add qq wechat

---
+++ manifests/com.tencent.WeChat/wechat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+setup_ime_env() {
+  if [[ "$XMODIFIERS" =~ fcitx ]]; then
+    [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=fcitx
+    [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=fcitx
+  elif [[ "$XMODIFIERS" =~ ibus ]]; then
+    [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
+    [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
+  fi
+}
+
+setup_ime_env
+
+export LD_PRELOAD=/app/lib/libredirect.so
+exec /app/extra/wechat/wechat "$@"
